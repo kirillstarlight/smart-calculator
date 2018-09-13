01b5b17702243a65d9bf50a86101f71fa21523b5
kirillstarlight <kirillstarlight@gmail.com>
2018-09-13 09:42:37 +0300

    pass

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue.toString();
   }
 
   add(number) {
-    // your implementation
+    this.initialValue += `+${number.toString()}`;
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.initialValue += `-${number.toString()}`;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.initialValue += `*${number.toString()}`;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.initialValue += `/${number.toString()}`;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.initialValue += `**${number.toString()}`;
+    return this;
+  }
+
+  toString() {
+    return eval(this.initialValue);
   }
 }
 
